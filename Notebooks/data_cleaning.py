6afbb6e4044a9842542deac1d29f76a46f1246e8
Daniel Bedmar <dbedmarr@gmail.com>
2025-04-09 13:08:58 +0200

    Fixed data cleaning and passed onto a .py file

---
+++ Notebooks/data_cleaning.py
@@ -0,0 +1,36 @@
+import polars as pl
+import os
+
+from utils.dictionaries import types_parque_original
+from utils.dictionaries import sub_tipo_mapping
+from utils.dictionaries import propulsion_mapping
+
+path = os.path.join("..","Data", "DGT")
+complete_park = os.path.join(path,'Parque_exacto','mat_2023.txt')
+clean_park = os.path.join(path,'Parque_exacto','clean_park.csv')
+
+parque = pl.scan_csv(complete_park, separator='|', schema=types_parque_original)
+
+parque = parque.filter(pl.col("FECHA_MATR") >= pl.col("FECHA_PRIM_MATR")).with_columns(
+    pl.col('POTENCIA').str.replace(",", ".").cast(pl.Float64),
+    pl.col('KW').str.replace(",", ".").cast(pl.Float64),
+    pl.col('SUBTIPO_DGT').replace(sub_tipo_mapping).alias("SUBTIPO_DGT"),
+    pl.col('PROPULSION').replace(propulsion_mapping).alias("PROPULSION"),
+    pl.col('AUTONOMIA').str.replace(" ", "").str.replace(",", ".").str.replace("0000Û0","000000").cast(pl.Float64))
+
+remolques = []
+for type in list(sub_tipo_mapping.values()):
+    if 'REMOLQUE' in type:
+        remolques.append(type)
+
+inverse_sub_tipo_mapping = {v: k for k, v in sub_tipo_mapping.items()}
+
+parque.with_columns(pl.when((pl.col("SUBTIPO_DGT").is_in(remolques)) |
+                            (pl.col('CLASE_MATR') == "Remolque"))
+                            .then(pl.lit("NO_EMISIONS"))
+                            .otherwise(pl.col("EMISIONES_EURO"))
+                            .alias("EMISIONES_EURO"),
+                    pl.col('SUBTIPO_DGT').replace(inverse_sub_tipo_mapping).alias("SUBTIPO_DGT"))
+
+park = parque.collect()
+park.write_csv(clean_park,separator='|')
